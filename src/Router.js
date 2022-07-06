8df53c6e742e0fbbdaf0bc9377206fc5fa509f35
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-06 16:15:41 +0900

    Modify: 2nd commit. 피드백 반영

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 
 // 민지's Components
-import LoginMinjee from "./pages/minjeeSon/Login/Login";
-import MainMinjee from "./pages/minjeeSon/Main/Main";
+import LoginMinjee from './pages/minjeeSon/Login/Login';
+import MainMinjee from './pages/minjeeSon/Main/Main';
 
 // 현민's Components
-import LoginHyeonmin from "./pages/hyeonminKil/Login/Login";
-import MainHyeonmin from "./pages/hyeonminKil/Main/Main";
+import LoginHyeonmin from './pages/hyeonminKil/Login/Login';
+import MainHyeonmin from './pages/hyeonminKil/Main/Main';
 
 // 성훈's Components
-import LoginSeonghun from "./pages/seonghunEom/Login/Login";
-import MainSeonghun from "./pages/seonghunEom/Main/Main";
+import LoginSeonghun from './pages/seonghunEom/Login/Login';
+import MainSeonghun from './pages/seonghunEom/Main/Main';
 
 // 원영's Components
-import LoginWonyoung from "./pages/wonyoungJoo/Login/Login";
-import MainWonyoung from "./pages/wonyoungJoo/Main/Main";
+import LoginWonyoung from './pages/wonyoungJoo/Login/Login';
+import MainWonyoung from './pages/wonyoungJoo/Main/Main';
 
 // import 한 컴포넌트에 대한 경로를 각각 설정해줍니다.
 const Router = () => {
