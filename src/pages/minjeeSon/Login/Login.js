989d49ca07e281638555614a882b637afdaf7a0d
Minjee Son <104430030+iamminzzy@users.noreply.github.com>
2022-07-08 15:08:33 +0900

    Add: Mission 2) Login | 로그인 버튼 활성화 (validation)

--- src/pages/minjeeSon/Login/Login.js
+++ src/pages/minjeeSon/Login/Login.js
@@ -4,11 +4,11 @@ import './Login.scss';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
-function LoginMinjee() {
+function Login() {
   //Main page로 이동
   const navigate = useNavigate();
   const goToMain = () => {
-    navigate('/main-minjeeSon');
+    navigate('/main');
   };
 
   //Mission 1) Login | 사용자 입력 데이터 저장
@@ -23,12 +23,21 @@ function LoginMinjee() {
     setPwInput(e.target.value);
   };
 
+  //Mission 2) Login | 로그인 버튼 활성화 (validation)
+  const [isValid, setIsValid] = useState(false);
+
+  const isValidLogin = () => {
+    return idInput.includes('@') && pwInput.length >= 5
+      ? setIsValid(true)
+      : setIsValid(false);
+  };
+
   return (
     <div className="container">
       <div className="logo">
         <h1>Westagram</h1>
       </div>
-      <form id="loginForm" className="login-form">
+      <form id="loginForm" className="login-form" onKeyUp={isValidLogin}>
         <input
           id="loginId"
           className="login-id"
@@ -45,9 +54,9 @@ function LoginMinjee() {
         />
         <button
           id="loginButton"
-          className="login-button"
+          className={'login-button' + (isValid ? ' active' : '')}
           type="button"
-          disabled
+          disabled={isValid ? false : true}
           onClick={goToMain}
         >
           로그인
@@ -60,4 +69,4 @@ function LoginMinjee() {
   );
 }
 
-export default LoginMinjee;
+export default Login;
